8d23777736ef900f16f76617cc1f2f56f7ea32be
umajho <umajho@proton.me>
2026-08-14 20:53:04 +0800

    style(openfx): remove unused commented code

--- crates/openfx/build.rs
+++ crates/openfx/build.rs
@@ -26,29 +26,4 @@ fn main() {
         vendor_c_include_dir,
         &generated_c_bindings_dir,
     );
-
-    // {
-    //     // The bindgen::Builder is the main entry point to bindgen, and lets you
-    //     // build up options for the resulting bindings.
-    //     let bindings = bindgen::Builder::default()
-    //         // The input header we would like to generate
-    //         // bindings for.
-    //         .header("wrapper-core.h")
-    //         .allowlist_function("") // blocking all
-    //         .allowlist_type("Ofx.+")
-    //         .allowlist_var("kOfx.+")
-    //         // Tell cargo to invalidate the built crate whenever any of the
-    //         // included header files changed.
-    //         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-    //         .generate_cstr(true)
-    //         // Finish the builder and generate the bindings.
-    //         .generate()
-    //         // Unwrap the Result and panic on failure.
-    //         .expect("Unable to generate bindings");
-
-    //     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    //     bindings
-    //         .write_to_file(generated_dir.join("core.rs"))
-    //         .expect("Couldn't write bindings!");
-    // }
 }
