36ed060c03d6d66b44fcd60414f059fa61878daf
umajho <umajho@proton.me>
2026-08-18 20:16:49 +0800

    refactor(openfx): apply 28c0a8f and c92cac8 to the other macro I forgot about

--- crates/openfx/src/internal/low_macros.rs
+++ crates/openfx/src/internal/low_macros.rs
@@ -12,12 +12,10 @@ pub macro make_enum_from_idents($name:ident, $($var:ident : $var_cstr:literal),*
         ///   value.
         pub unsafe fn from_ptr(ptr: *const std::os::raw::c_char) -> Self {
             let cstr = unsafe { std::ffi::CStr::from_ptr(ptr) };
-            $(
-                if cstr == $var_cstr {
-                    return Self::$var;
-                }
-            )*
-            Self::Other(cstr.as_ptr())
+            match true {
+                $(_ if cstr == $var_cstr => Self::$var,)*
+                _ => Self::Other(cstr.as_ptr()),
+            }
         }
 
         /// ## SAFETY
@@ -25,15 +23,10 @@ pub macro make_enum_from_idents($name:ident, $($var:ident : $var_cstr:literal),*
         /// The returned pointer is valid as long as the [`std::ffi::CString`]
         /// inside [`Self::Other`] is not dropped.
         pub fn as_ptr(&self) -> *const std::os::raw::c_char {
-            if let Self::Other(ptr) = self {
-                return *ptr;
+            match self {
+                $(Self::$var => $var_cstr.as_ptr(),)*
+                Self::Other(ptr) => *ptr,
             }
-            $(
-                if matches!(self, Self::$var) {
-                    return $var_cstr.as_ptr() as *const std::os::raw::c_char;
-                }
-            )*
-            unreachable!()
         }
     }
 }
