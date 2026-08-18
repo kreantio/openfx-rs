28c0a8feaef699e936a5316701eb9c2ca91182a0
umajho <umajho@proton.me>
2026-08-18 17:36:46 +0800

    refactor(openfx): found out that repetitions can be expanded in match bodies

--- crates/openfx/generated/code_from_cpp/low_enums.rs
+++ crates/openfx/generated/code_from_cpp/low_enums.rs
@@ -49,27 +49,20 @@ macro make_enum_from_paths($name:ident, $(#[$meta:meta] $var:ident => $var_path:
     /// - The pointer must live at least as long as the returned [`Self`] value.
     pub unsafe fn from_ptr(ptr: *const std::os::raw::c_char) -> Self {
       let cstr = unsafe { std::ffi::CStr::from_ptr(ptr) };
-      $(
-        if cstr == $var_path {
-          return Self::$var;
-        }
-      )*
-      Self::Other(cstr.as_ptr())
+      match true {
+        $(_ if cstr == $var_path => Self::$var,)*
+        _ => Self::Other(cstr.as_ptr()),
+      }
     }
 
     /// ## SAFETY
     ///
     /// The returned pointer is valid as long as the [`std::ffi::CString`] inside [`Self::Other`] is not dropped.
     pub fn as_ptr(&self) -> *const std::os::raw::c_char {
-      if let Self::Other(ptr) = self {
-        return *ptr;
+      match self {
+        $(Self::$var => $var_path.as_ptr() as *const std::os::raw::c_char,)*
+        Self::Other(ptr) => *ptr,
       }
-      $(
-        if matches!(self, Self::$var) {
-          return $var_path.as_ptr() as *const std::os::raw::c_char;
-        }
-      )*
-      unreachable!()
     }
   }
 }
