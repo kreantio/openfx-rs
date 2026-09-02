4730ac698a3187902ec68376a8bdcbad8e1ed373
umajho <umajho@proton.me>
2026-09-03 00:56:36 +0800

    feat(openfx-internal-macros): add sys_match/matches methods to low enums

--- crates/openfx-internal-macros/src/low_macros/make_property_enums.rs
+++ crates/openfx-internal-macros/src/low_macros/make_property_enums.rs
@@ -26,9 +26,17 @@ pub fn make_property_enums(tokens: TokenStream) -> TokenStream {
                 let var_name = variant.name();
                 inner.extend(quote! { #var_name, });
             }
-            inner.extend(quote! { Other(*const std::os::raw::c_char), });
+            inner.extend(quote! {
+                /// Use [`Self::matches`] instead. Even if matching this variant
+                /// for unrecognized values works now, it will break when this
+                /// crate adds dedicated variants for those values in a future
+                /// release.
+                UnknownDontUseThisDirectly(*const std::os::raw::c_char),
+            });
             output.extend(quote! {
-                #[derive(Debug, Clone, PartialEq, Eq)] pub enum #name { #inner }
+                #[derive(Debug, Clone, PartialEq, Eq)]
+                #[non_exhaustive]
+                pub enum #name { #inner }
             });
         }
 
@@ -64,7 +72,7 @@ pub fn make_property_enums(tokens: TokenStream) -> TokenStream {
                         let cstr = unsafe { std::ffi::CStr::from_ptr(ptr) };
                         match true {
                             #( _ if cstr == #vals => Self::#vars, )*
-                            _ => Self::Other(cstr.as_ptr()),
+                            _ => Self::UnknownDontUseThisDirectly(cstr.as_ptr()),
                         }
                     }
 
@@ -76,7 +84,7 @@ pub fn make_property_enums(tokens: TokenStream) -> TokenStream {
                     ///   [`Self`] value.
                     pub unsafe fn from_ptr_null_checked(ptr: *const std::os::raw::c_char) -> Self {
                         if ptr.is_null() {
-                            Self::Other(std::ptr::null())
+                            Self::UnknownDontUseThisDirectly(std::ptr::null())
                         } else {
                             Self::from_ptr(ptr)
                         }
@@ -85,11 +93,27 @@ pub fn make_property_enums(tokens: TokenStream) -> TokenStream {
                     /// ## SAFETY
                     ///
                     /// The returned pointer is valid as long as the
-                    /// [`std::ffi::CString`] inside [`Self::Other`] is not dropped.
+                    /// [`std::ffi::CString`] inside
+                    /// [`Self::UnknownDontUseThisDirectly`] is not dropped.
                     pub fn as_ptr(&self) -> *const std::os::raw::c_char {
                         match self {
                             #( Self::#vars => #vals.as_ptr(), )*
-                            Self::Other(ptr) => *ptr,
+                            Self::UnknownDontUseThisDirectly(ptr) => *ptr,
+                        }
+                    }
+
+                    /// ## SAFETY
+                    ///
+                    /// - The pointer must be either null or valid and point to
+                    ///   a null-terminated C string.
+                    /// - The pointer must live at least as long as the returned
+                    ///   [`Self`] value.
+                    pub unsafe fn matches(&self, val: &::std::ffi::CStr) -> bool {
+                        let ptr = self.as_ptr();
+                        if ptr.is_null() {
+                            false
+                        } else {
+                            ::std::ffi::CStr::from_ptr(ptr) == val
                         }
                     }
                 }
