fc1ed71b1242c2b3bc2982b09b5ca834f0a17b20
umajho <umajho@proton.me>
2026-09-02 22:25:27 +0800

    fix(examples/ofx-guide-example-plugins-layer-low): forgot to change definitions
    of plugin identifiers and labels

--- examples/ofx-guide-example-plugins-layer-low/src/definitions.rs
+++ examples/ofx-guide-example-plugins-layer-low/src/definitions.rs
@@ -3,21 +3,21 @@ use std::ffi::CStr;
 pub const PLUGINS_GROUPING: &CStr = c"openfx-rs: OFX Guide Example";
 
 pub const PLUGIN_1_BASICS_IDENTIFIER: &CStr =
-    c"umajho.kreantio.openfx-rs.ofx-guide-example-plugins.layer-sys:BasicExamplePlugin";
-pub const PLUGIN_1_BASICS_LABEL: &CStr = c"[layer sys] OFX Basics Example";
+    c"umajho.kreantio.openfx-rs.ofx-guide-example-plugins.layer-low:BasicExamplePlugin";
+pub const PLUGIN_1_BASICS_LABEL: &CStr = c"[layer low] OFX Basics Example";
 
 pub const PLUGIN_2_INVERT_IDENTIFIER: &CStr =
-    c"umajho.kreantio.openfx-rs.ofx-guide-example-plugins.layer-sys:InvertExamplePlugin";
-pub const PLUGIN_2_INVERT_LABEL: &CStr = c"[layer sys] OFX Invert Example";
+    c"umajho.kreantio.openfx-rs.ofx-guide-example-plugins.layer-low:InvertExamplePlugin";
+pub const PLUGIN_2_INVERT_LABEL: &CStr = c"[layer low] OFX Invert Example";
 
 pub const PLUGIN_3_GAIN_IDENTIFIER: &CStr =
-    c"umajho.kreantio.openfx-rs.ofx-guide-example-plugins.layer-sys:GainExamplePlugin";
-pub const PLUGIN_3_GAIN_LABEL: &CStr = c"[layer sys] OFX Gain Example";
+    c"umajho.kreantio.openfx-rs.ofx-guide-example-plugins.layer-low:GainExamplePlugin";
+pub const PLUGIN_3_GAIN_LABEL: &CStr = c"[layer low] OFX Gain Example";
 
 pub const PLUGIN_4_SATURATION_IDENTIFIER: &CStr =
-    c"umajho.kreantio.openfx-rs.ofx-guide-example-plugins.layer-sys:SaturationExamplePlugin";
-pub const PLUGIN_4_SATURATION_LABEL: &CStr = c"[layer sys] OFX Saturation Example";
+    c"umajho.kreantio.openfx-rs.ofx-guide-example-plugins.layer-low:SaturationExamplePlugin";
+pub const PLUGIN_4_SATURATION_LABEL: &CStr = c"[layer low] OFX Saturation Example";
 
 pub const PLUGIN_5_CIRCLE_IDENTIFIER: &CStr =
-    c"umajho.kreantio.openfx-rs.ofx-guide-example-plugins.layer-sys:CircleExamplePlugin";
-pub const PLUGIN_5_CIRCLE_LABEL: &CStr = c"[layer sys] OFX Circle Example";
+    c"umajho.kreantio.openfx-rs.ofx-guide-example-plugins.layer-low:CircleExamplePlugin";
+pub const PLUGIN_5_CIRCLE_LABEL: &CStr = c"[layer low] OFX Circle Example";
